4f6c5c3e426ca707de929a0dc5ab392771404c86
Conrad Indiono <indygemma@gmail.com>
2010-09-28 02:00:08 +0200

    * added "tmp" dir creation if on running supervisor it doesn't exist

--- dev/runsupervisor.py
+++ dev/runsupervisor.py
@@ -7,6 +7,9 @@ sys.path.insert(0, pwd)
 
 from lib.cmd import valid_settings, run_supervisord
 
+if not os.path.exists(os.path.join(pwd, "tmp")):
+	os.mkdir(os.path.join(pwd, "tmp"))
+
 if __name__ == '__main__':
 	if len(sys.argv) < 2:
 		print "Usage: runsupervisor.py <configuration file>"
