0a49e4225127f9a0547faf9c5d315c27e4ee36ea
Conrad Indiono <indygemma@gmail.com>
2010-09-28 03:39:25 +0200

    * changed media url to use nginx's port

--- settings/production.py
+++ settings/production.py
@@ -20,7 +20,7 @@ DATABASES = {
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = 'http://127.0.0.1:8080/media/'
+MEDIA_URL = 'http://65.49.73.73:9000/media/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
