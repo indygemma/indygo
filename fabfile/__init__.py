4b1c377eb5d6f5decec1e3e083eb07f03a9a48bd
Conrad Indiono <indygemma@gmail.com>
2010-09-28 14:42:54 +0200

    * cleaned up fabfile in preperation to convert this as a template

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -1,6 +1,13 @@
 from fabric.api import *
+import os
 
-env.hosts = ["root@65.49.73.73"]
+USER = "root"
+env.hosts = ["%s@65.49.73.73" % USER]
+PROJECT_NAME = "sample"
+REPO_DIR  = os.path.join("/home", USER, "repos")
+REPO_PATH = os.path.join(REPO_DIR, "%s.git" % PROJECT_NAME)
+SITE_DIR  = os.path.join("/home", USER, "sites")
+SITE_PATH = os.path.join(SITE_DIR, PROJECT_NAME)
 
 def host_type():
 	run("uname -s")
@@ -18,62 +25,72 @@ def install_libevent():
 	# TODO: on servers without root access I'll have to statically compile gevent
 	# run("apt-get install libevent-dev")
 
-def setup_git():
+def setup_repo():
 	""" create an empty git repo on the server and push the local git repo to that """
-	giturl = "%(user)s@%(host)s:~/repos/sample.git" % env
-	run("mkdir -p repos/sample.git")
-	with cd("repos/sample.git"):
+	giturl = "%(user)s@%(host)s:%(REPO_PATH)s" % {"user":env.user, "host":env.host, "REPO_PATH": REPO_PATH}
+	run("mkdir -p %s" % REPO_PATH)
+	with cd(REPO_PATH):
 		run("git init --bare")
 	local("git remote add production %(giturl)s" % locals())
 	local("git push production master")
 	
-def remove_git():
+def remove_repo():
 	""" remove the remote repo and the local reference to that repo """
 	local("git remote rm production")
-	run("rm -rf repos/sample.git")
+	run("rm -rf %s" % REPO_PATH)
 
 def setup_site():
-	run("mkdir -p sites")
-	with cd("sites"):
-		run("git clone ~/repos/sample.git")
+	run("mkdir -p %s" % SITE_DIR)
+	with cd(SITE_DIR):
+		run("git clone %s" % REPO_PATH)
 
 def remove_site():
-	run("rm -rf sites/sample")
+	run("rm -rf %s" % SITE_PATH)
+
+def clean():
+	""" remove git repo and site directory """
+	remove_repo()
+	remove_site()
 
 def bootstrap():
 	""" setup the whole site environment """
-	setup_git()
+	setup_repo()
 	setup_site()
-	with cd("sites/sample"):
+	with cd(SITE_PATH):
 		run("./bootstrap/bootstrap.py") # requires install_libevent for gevent
 
 def run_pip_freeze():
-	with cd("sites/sample/virtualenv/bin"):
+	with cd(os.path.join(SITE_PATH, "virtualenv", "bin")):
 		run("./pip freeze")
 
 def run_wsgi():
-	with cd("sites/sample"):
+	with cd(SITE_PATH):
 		run("./dev/runwsgi.py production")
 
 def restart_server():
-	run("pkill sample") # all wsgi servers run as this. set in dev/runwsgi.py via setproctitle
+	with settings(warn_only=True):
+		result = run("pkill %s" % PROJECT_NAME) # all wsgi servers run as this. set in dev/runwsgi.py via setproctitle
+	if result.failed:
+		print "There's no server running. Ignoring kill request"
 
 def run_supervisor():
-	with cd("sites/sample"):
+	with cd(SITE_PATH):
 		run("./dev/runsupervisor.py production")
 
 def kill_supervisor():
-	with cd("sites/sample"):
+	with cd(SITE_PATH):
 		run("./dev/killsupervisor.py")
 
+stop = kill_supervisor # alias for kill_supervisor
+
 def enable_nginx(): # requires install_nginx
-	with cd("sites/sample"):
+	with cd(SITE_PATH):
 		run("./etc/nginx/enable.py production")
 		run("/etc/init.d/nginx restart")
 
 def push():
 	local("git push production")
-	with cd("sites/sample"):
+	with cd(SITE_PATH):
 		run("git pull")
 
 def deploy():
